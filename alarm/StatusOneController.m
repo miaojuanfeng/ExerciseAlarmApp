19c9989cf605af20b05a96e99d5e96b88a72ff6f
Dreamover Studio <dreamover@Don.local>
2018-02-25 21:41:54 +0800

    add calandar and pt

--- alarm/StatusOneController.m
+++ alarm/StatusOneController.m
@@ -9,9 +9,10 @@
 #import <Foundation/Foundation.h>
 
 #import "StatusOneController.h"
+#import "PTHistogramView.h"
 
 @interface StatusOneController ()
-
+@property (nonatomic, strong) PTHistogramView *ptView;
 @end
 
 @implementation StatusOneController
@@ -22,11 +23,27 @@
     self.view.backgroundColor = [UIColor whiteColor];
     self.navigationItem.title = @"累積鍛煉";
     
-    UILabel *alarmTime = [[UILabel alloc] init];
-    alarmTime.frame = CGRectMake(self.view.frame.size.width/2 - 150, 50, 300, 100);
-    alarmTime.text = @"版本更新頁";
-    alarmTime.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:alarmTime];
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(20, 74, self.view.frame.size.width-40, 50)];
+    title.text = @"累積鍛煉";
+    title.font = [UIFont fontWithName:@"AppleGothic" size:16.0];
+    [self.view addSubview:title];
+    
+    UILabel *num = [[UILabel alloc] initWithFrame:CGRectMake(20, 134, self.view.frame.size.width-40, 50)];
+    num.textAlignment = NSTextAlignmentCenter;
+    
+    NSMutableAttributedString *str = [[NSMutableAttributedString alloc] initWithString:@"123 分鐘"];
+    [str addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"AppleGothic" size:36.0] range:NSMakeRange(0,str.length-2)];
+    num.attributedText = str;
+    
+    [self.view addSubview:num];
+    
+    
+    UIView *calendarView = [[UIView alloc] initWithFrame:CGRectMake(0, 120, self.view.frame.size.width, self.view.frame.size.height-120)];
+    _ptView = [[PTHistogramView alloc] initWithFrame:CGRectMake(30, 100, [UIScreen mainScreen].bounds.size.width - 60, 200)
+                                           nameArray:@[@"2/1",@"2/2",@"2/3",@"2/4",@"2/5"]
+                                          countArray:@[@"4",@"12",@"7",@"15",@"19"]];
+    [calendarView addSubview:_ptView];
+    [self.view addSubview:calendarView];
 }
 
 
--- alarm/StatusThreeController.m
+++ alarm/StatusThreeController.m
@@ -9,8 +9,19 @@
 #import <Foundation/Foundation.h>
 
 #import "StatusThreeController.h"
+#import "YXCalendarView.h"
 
-@interface StatusThreeController ()
+@interface StatusThreeController () <UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate>
+
+@property (nonatomic, strong) YXCalendarView *calendar;
+
+@property (nonatomic, strong) UITableView *tableView;
+//获取投资日历的高度
+@property (nonatomic, assign) CGFloat calendarHeight;
+//数据
+@property (nonatomic, strong) NSString *data;
+//每次用户拖动tableView的时候，只能发送一次让tableView的header收起和展开的通知
+@property (nonatomic, assign) BOOL isAllowPostNoti;
 
 @end
 
@@ -22,17 +33,127 @@
     self.view.backgroundColor = [UIColor whiteColor];
     self.navigationItem.title = @"累計天數";
     
-    UILabel *alarmTime = [[UILabel alloc] init];
-    alarmTime.frame = CGRectMake(self.view.frame.size.width/2 - 150, 50, 300, 100);
-    alarmTime.text = @"版本更新頁";
-    alarmTime.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:alarmTime];
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(20, 74, self.view.frame.size.width-40, 50)];
+    title.text = @"累計天數";
+    title.font = [UIFont fontWithName:@"AppleGothic" size:16.0];
+    [self.view addSubview:title];
+    
+    UILabel *num = [[UILabel alloc] initWithFrame:CGRectMake(20, 134, self.view.frame.size.width-40, 50)];
+    num.textAlignment = NSTextAlignmentCenter;
+    
+    NSMutableAttributedString *str = [[NSMutableAttributedString alloc] initWithString:@"63 天"];
+    [str addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"AppleGothic" size:36.0] range:NSMakeRange(0,str.length-1)];
+    num.attributedText = str;
+    
+    [self.view addSubview:num];
+    
+    UIView *calendarView = [[UIView alloc] initWithFrame:CGRectMake(0, 140, self.view.frame.size.width, self.view.frame.size.height-140)];
+    //加载投资日历
+    [calendarView addSubview:self.calendar];
+    [self.view addSubview:calendarView];
+    //加载每日日历内容
+//    [self.view addSubview:self.tableView];
+//    self.tableView.frame = CGRectMake(0, 64+self.calendarHeight, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-64-self.calendarHeight);
+    
+    self.isAllowPostNoti = YES;
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+/**
+ *tableView的懒加载
+ */
+- (UITableView *)tableView{
+    if(!_tableView){
+        _tableView = [[UITableView alloc] init];
+        
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        
+        _tableView.tableFooterView = [UIView new];
+        _tableView.showsVerticalScrollIndicator = NO;
+        _tableView.showsHorizontalScrollIndicator = NO;
+        _tableView.userInteractionEnabled = YES;
+        
+    }
+    return _tableView;
+}
+/**
+ *  日历的懒加载
+ */
+- (YXCalendarView *)calendar{
+    if(!_calendar){
+        _calendar = [[YXCalendarView alloc] initWithFrame:CGRectMake(0, 64, [UIScreen mainScreen].bounds.size.width, [YXCalendarView getMonthTotalHeight:[NSDate date] type:CalendarType_Month]) Date:[NSDate date] Type:CalendarType_Month];
+        self.calendarHeight = [YXCalendarView getMonthTotalHeight:[NSDate date] type:CalendarType_Month];
+        __weak typeof (self) WeakSelf = self;
+        //改变日历头部和tableView 的cell位置
+        [self changeLocation];
+        //点击日历某一个日期  进行数据刷新
+        _calendar.sendSelectDate = ^(NSDate *selDate) {
+            [WeakSelf serviceDataByData:[[YXDateHelpObject manager] getStrFromDateFormat:@"yyyy-MM-dd" Date:selDate]];
+        };
+    }
+    return _calendar;
+}
+//请求数据
+- (void)serviceDataByData:(NSString *)data{
+    self.data = @"1";
+    
+    [self.tableView reloadData];
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if (self.data.intValue == 1) {
+        return 10;
+    }else{
+        return 10;
+    }
+}
+/**
+ *  改变日历头部和tableView 的cell位置
+ */
+- (void)changeLocation{
+    __weak typeof(_calendar) weakCalendar = _calendar;
+    __weak typeof (self) WeakSelf = self;
+    _calendar.refreshH = ^(CGFloat viewH) {
+        WeakSelf.calendarHeight = viewH;
+        [UIView animateWithDuration:0.3 animations:^{
+            weakCalendar.frame = CGRectMake(0, 64, [UIScreen mainScreen].bounds.size.width, viewH);
+            WeakSelf.tableView.frame = CGRectMake(0, 64+viewH, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-64-viewH);
+        }];
+    };
+}
+/**
+ *  通过监听tableViewcell的偏移量 从而判断tableView的头部应该收缩或者伸展
+ */
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    
+    if (scrollView.contentOffset.y > 0) {
+        NSNotification *notifi = [[NSNotification alloc] initWithName:@"changeHeaderHeightToLow" object:nil userInfo:nil];
+        [[NSNotificationCenter defaultCenter] postNotification:notifi];
+    }else if(scrollView.contentOffset.y < 0){
+        NSNotification *notifi = [[NSNotification alloc] initWithName:@"changeHeaderHeightToHeigh" object:nil userInfo:nil];
+        [[NSNotificationCenter defaultCenter] postNotification:notifi];
+    }
+}
+
+#pragma mark - cellForRowAtIndexPath
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString *str = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:str];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:str];
+    }
+    if ([self.data isEqualToString:@"1"]) {
+        cell.textLabel.text = @"66666";
+    }else{
+        cell.textLabel.text = @"444444";
+    }
+    
+    return cell;
 }
 
 @end
