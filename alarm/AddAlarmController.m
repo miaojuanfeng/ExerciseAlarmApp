dc09e725f44613eea0d24ac42b02702260432aa7
miaojuanfeng <miaojuanfeng@qq.com>
2018-02-27 16:56:38 +0800

    asdasdasdasd

--- alarm/AddAlarmController.m
+++ alarm/AddAlarmController.m
@@ -22,9 +22,11 @@
 //@property UIImageView *imageView;
 @property NSString *photoName;
 @property unsigned int soundId;
+@property NSString *alarmTitle;
 @property UIActivityIndicatorView *activityIndicator;
 @property UIButton *photoButton;
 @property UIButton *soundButton;
+@property UILabel *titleLabel;
 @end
 
 @implementation AddAlarmController
@@ -53,6 +55,7 @@
     if( self.soundId == 0 ){
         self.soundId = 1000;
     }
+    self.alarmTitle = @"鍛煉提醒";
     
     
 //    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 280, self.view.frame.size.width, self.view.frame.size.height-280)];
@@ -75,6 +78,10 @@
     [self.soundButton setTitle:@"試聽" forState:UIControlStateNormal];
     [self.soundButton addTarget:self action:@selector(clickShowSoundButton) forControlEvents:UIControlEventTouchUpInside];
     [self.soundButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+    
+    self.titleLabel = [[UILabel alloc] init];
+    self.titleLabel.textColor = [UIColor lightGrayColor];
+    self.titleLabel.textAlignment = NSTextAlignmentRight;
 }
 
 
@@ -84,85 +91,129 @@
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return 2;
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    switch (section) {
+        case 0:
+            return 2;
+            break;
+        case 1:
+            return 1;
+            break;
+    }
+    return 0;
+}
+
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    return @"基本設置";
+    switch (section) {
+        case 0:
+            return @"基本設置";
+            break;
+        case 1:
+            return @"更多設置";
+            break;
+    }
+    return nil;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [[UITableViewCell alloc] init];
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-    switch ( indexPath.row ) {
-        case 0:
-            cell.textLabel.text = @"圖片";
-            cell.imageView.image = [UIImage imageNamed:@"gallery"];
-            self.photoButton.frame = CGRectMake(cell.frame.size.width - 20, 0, 40, cell.frame.size.height);
-            break;
-        case 1:
-            cell.textLabel.text = @"鈴聲";
-            cell.imageView.image = [UIImage imageNamed:@"music"];
-            self.soundButton.frame = CGRectMake(cell.frame.size.width - 20, 0, 40, cell.frame.size.height);
-            break;
+    if( indexPath.section == 0 ){
+        switch ( indexPath.row ) {
+            case 0:
+                cell.textLabel.text = @"圖片";
+                cell.imageView.image = [UIImage imageNamed:@"gallery"];
+                self.photoButton.frame = CGRectMake(cell.frame.size.width - 20, 0, 40, cell.frame.size.height);
+                break;
+            case 1:
+                cell.textLabel.text = @"鈴聲";
+                cell.imageView.image = [UIImage imageNamed:@"music"];
+                self.soundButton.frame = CGRectMake(cell.frame.size.width - 20, 0, 40, cell.frame.size.height);
+                break;
+        }
+    }else if( indexPath.section == 1 ){
+        switch ( indexPath.row ) {
+            case 0:
+                cell.textLabel.text = @"標題";
+                cell.imageView.image = [UIImage imageNamed:@"settings"];
+                self.titleLabel.frame = CGRectMake(cell.frame.size.width - 83, 0, 100, cell.frame.size.height);
+                break;
+        }
     }
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
     SelectMusicController *selectMusicController = nil;
-    if (indexPath.row == 0) {
-            // selectPhotoController = [self.storyboard instantiateViewControllerWithIdentifier:@"SelectPhotoController"];
-            // [self.navigationController pushViewController:selectPhotoController animated:YES];
-//            pickerController = [[UIImagePickerController alloc] init];
-//            pickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-////            pickerController.allowsEditing = YES;
-//            pickerController.delegate = self;
-//            [self presentViewController:pickerController animated:YES completion:nil];
-            self.actionSheet = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
-            UIAlertAction *cameraAction = [UIAlertAction actionWithTitle:@"拍攝" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-                if( [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera] ){
-                    [self.activityIndicator startAnimating];
-                    UIImagePickerController *pickerController = [[UIImagePickerController alloc] init];
-                    pickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
-                    //            pickerController.allowsEditing = YES;
-                    pickerController.delegate = self;
-                    [self presentViewController:pickerController animated:YES completion:nil];
-                }else{
-                    NSLog(@"不支持相机");
-                }
-            }];
-            UIAlertAction *photoLibraryAction = [UIAlertAction actionWithTitle:@"從手機相冊選擇" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-                if( [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary] ){
-                    [self.activityIndicator startAnimating];
-                    UIImagePickerController *pickerController = [[UIImagePickerController alloc] init];
-                    pickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-                    //            pickerController.allowsEditing = YES;
-                    pickerController.delegate = self;
-                    [self presentViewController:pickerController animated:YES completion:nil];
-                }else{
-                    NSLog(@"不支持图库");
-                }
-            }];
-            UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+    if( indexPath.section == 0 ){
+        if (indexPath.row == 0) {
+                self.actionSheet = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+                UIAlertAction *cameraAction = [UIAlertAction actionWithTitle:@"拍攝" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                    if( [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera] ){
+                        [self.activityIndicator startAnimating];
+                        UIImagePickerController *pickerController = [[UIImagePickerController alloc] init];
+                        pickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+                        //            pickerController.allowsEditing = YES;
+                        pickerController.delegate = self;
+                        [self presentViewController:pickerController animated:YES completion:nil];
+                    }else{
+                        NSLog(@"不支持相机");
+                    }
+                }];
+                UIAlertAction *photoLibraryAction = [UIAlertAction actionWithTitle:@"從手機相冊選擇" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                    if( [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary] ){
+                        [self.activityIndicator startAnimating];
+                        UIImagePickerController *pickerController = [[UIImagePickerController alloc] init];
+                        pickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+                        //            pickerController.allowsEditing = YES;
+                        pickerController.delegate = self;
+                        [self presentViewController:pickerController animated:YES completion:nil];
+                    }else{
+                        NSLog(@"不支持图库");
+                    }
+                }];
+                UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
 
-            }];
-            [cancelAction setValue:[UIColor redColor] forKey:@"_titleTextColor"];
-            [self.actionSheet addAction:cameraAction];
-            [self.actionSheet addAction:photoLibraryAction];
-            [self.actionSheet addAction:cancelAction];
-            [self presentViewController:self.actionSheet animated:YES completion:^{
-                
-            }];
-    }else if(indexPath.row == 1){
-            selectMusicController = [[SelectMusicController alloc] init];
-            selectMusicController.soundId = self.soundId;
-            selectMusicController.delegate = self;
-            [self.navigationController pushViewController:selectMusicController animated:YES];
+                }];
+                [cancelAction setValue:[UIColor redColor] forKey:@"_titleTextColor"];
+                [self.actionSheet addAction:cameraAction];
+                [self.actionSheet addAction:photoLibraryAction];
+                [self.actionSheet addAction:cancelAction];
+                [self presentViewController:self.actionSheet animated:YES completion:^{
+                    
+                }];
+        }else if(indexPath.row == 1){
+                selectMusicController = [[SelectMusicController alloc] init];
+                selectMusicController.soundId = self.soundId;
+                selectMusicController.delegate = self;
+                [self.navigationController pushViewController:selectMusicController animated:YES];
+        }
+    }else if( indexPath.section == 1 ){
+        if( indexPath.row == 0 ){
+                self.actionSheet = [UIAlertController alertControllerWithTitle:@"請輸入標題" message:nil preferredStyle:UIAlertControllerStyleAlert];
+                [self.actionSheet addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+                    textField.placeholder = @"請輸入標題";
+                }];
+                __weak typeof(self.actionSheet) weakAlert = self.actionSheet;
+                UIAlertAction *sureAction = [UIAlertAction actionWithTitle:@"確定" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+                    self.alarmTitle = weakAlert.textFields.firstObject.text;
+                    self.titleLabel.text = weakAlert.textFields.firstObject.text;
+                    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:1];
+                    UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+                    [cell.contentView addSubview:self.titleLabel];
+                }];
+                UIAlertAction *cacleAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+                    
+                }];
+                [self.actionSheet addAction:sureAction];
+                [self.actionSheet addAction:cacleAction];
+                [self presentViewController:self.actionSheet animated:YES completion:^{
+                    
+                }];
+        }
     }
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
@@ -183,6 +234,7 @@
         
         //拿到图片
         UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+        image = [self fixOrientation:image];
         //设置一个图片的存储路径
         NSString *imagePath = [NSString stringWithFormat:@"%@/Documents/%@",NSHomeDirectory(),timeStampString];
         //把图片直接保存到指定的路径（同时应该把图片的路径imagePath存起来，下次就可以直接用来取）
@@ -226,7 +278,7 @@
     //赋值
     [newsDict setObject:hh forKey:@"hour"];
     [newsDict setObject:mm forKey:@"minute"];
-    [newsDict setObject:@"鍛煉提醒" forKey:@"title"];
+    [newsDict setObject:self.alarmTitle forKey:@"title"];
     [newsDict setObject:self.photoName forKey:@"photo"];
     [newsDict setObject:[NSString stringWithFormat:@"%d", self.soundId] forKey:@"sound"];
     [newsDict setObject:@"1" forKey:@"status"];
@@ -282,7 +334,9 @@
         [newsArr writeToFile:plistPath atomically:YES];
         
         UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"運動鬧鐘" message:@"成功添加鬧鐘" preferredStyle:UIAlertControllerStyleAlert];
-        UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"確認" style:UIAlertActionStyleCancel handler:nil];
+        UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"確認" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+            [self.navigationController popViewControllerAnimated:YES];
+        }];
         [alert addAction:confirmAction];
         [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
     }];
@@ -308,6 +362,83 @@
 
 - (void)clickShowSoundButton {
     AudioServicesPlaySystemSound(self.soundId);
+}
+
+- (UIImage *)fixOrientation:(UIImage *)aImage {
+    
+    // No-op if the orientation is already correct
+    if (aImage.imageOrientation == UIImageOrientationUp)
+        return aImage;
+    
+    // We need to calculate the proper transformation to make the image upright.
+    // We do it in 2 steps: Rotate if Left/Right/Down, and then flip if Mirrored.
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    
+    switch (aImage.imageOrientation) {
+        case UIImageOrientationDown:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, aImage.size.width, aImage.size.height);
+            transform = CGAffineTransformRotate(transform, M_PI);
+            break;
+            
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+            transform = CGAffineTransformTranslate(transform, aImage.size.width, 0);
+            transform = CGAffineTransformRotate(transform, M_PI_2);
+            break;
+            
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, 0, aImage.size.height);
+            transform = CGAffineTransformRotate(transform, -M_PI_2);
+            break;
+        default:
+            break;
+    }
+    
+    switch (aImage.imageOrientation) {
+        case UIImageOrientationUpMirrored:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, aImage.size.width, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+            
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, aImage.size.height, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+        default:
+            break;
+    }
+    
+    // Now we draw the underlying CGImage into a new context, applying the transform
+    // calculated above.
+    CGContextRef ctx = CGBitmapContextCreate(NULL, aImage.size.width, aImage.size.height,
+                                             CGImageGetBitsPerComponent(aImage.CGImage), 0,
+                                             CGImageGetColorSpace(aImage.CGImage),
+                                             CGImageGetBitmapInfo(aImage.CGImage));
+    CGContextConcatCTM(ctx, transform);
+    switch (aImage.imageOrientation) {
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            // Grr...
+            CGContextDrawImage(ctx, CGRectMake(0,0,aImage.size.height,aImage.size.width), aImage.CGImage);
+            break;
+            
+        default:
+            CGContextDrawImage(ctx, CGRectMake(0,0,aImage.size.width,aImage.size.height), aImage.CGImage);
+            break;
+    }
+    
+    // And now we just create a new UIImage from the drawing context
+    CGImageRef cgimg = CGBitmapContextCreateImage(ctx);
+    UIImage *img = [UIImage imageWithCGImage:cgimg];
+    CGContextRelease(ctx);
+    CGImageRelease(cgimg);
+    return img;
 }
 
 @end
